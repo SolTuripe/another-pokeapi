0f2b1621400ecfa272a5523f51b2ca3b8948284a
Sol Turipe <solcis.bio@gmail.com>
2022-02-14 10:51:34 +0100

    pokemon object function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { useEffect, useState } from "react";
 
-function App() {
-  const [allPokemons, setAllPokemos] = useState([]);
+const App = () => {
+  const [allPokemons, setAllPokemons] = useState([]);
   const [loadMore, setLoadMore] = useState("https://pokeapi.co/api/v2/pokemon");
 
   const getAllPokemons = async () => {
     const res = await fetch(loadMore);
     const data = await res.json();
 
-    console.log(data);
+    setLoadMore(data.next);
+
+    function createPokemonObject(result) {
+      result.forEach(async (pokemon) => {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        const data = await res.json();
+        setAllPokemons((currentList) => [...currentList, data]);
+      });
+    }
+
+    createPokemonObject(data.results);
+    await console.log(allPokemons);
   };
 
   useEffect(() => {
@@ -24,6 +37,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
