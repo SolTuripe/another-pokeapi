92fbad27b6457acab2983f5ee81d299a5dc7692c
Sol Turipe <solcis.bio@gmail.com>
2022-02-14 10:05:56 +0100

    principal structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [allPokemons, setAllPokemos] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>PokeApi</h1>
+      <div className="pokemon-container">
+        <div className="all-container"></div>
+        <button className="load-more">More Pokemons</button>
+      </div>
     </div>
   );
 }
