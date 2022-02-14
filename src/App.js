ff4f8d3c6ce75f847a84715e67e846e0b0d0660b
Sol Turipe <solcis.bio@gmail.com>
2022-02-14 10:20:45 +0100

    getAllPokemons function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [allPokemons, setAllPokemos] = useState([]);
+  const [loadMore, setLoadMore] = useState("https://pokeapi.co/api/v2/pokemon");
+
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore);
+    const data = await res.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
 
   return (
     <div className="app-container">
